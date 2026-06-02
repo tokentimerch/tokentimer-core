ac8bc452828abf7639b40a8be65da6327accc6a5
maxime zupka <zupkamaxime@gmail.com>
2026-06-02 18:40:56 +0200

    fix(worker): stop dev worker on interrupt

--- scripts/run-with-env.js
+++ scripts/run-with-env.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { loadRootEnv } = require("./load-root-env");
-const { spawnCommand } = require("./process-utils");
+const { killProcessTree, spawnCommand } = require("./process-utils");
 
 loadRootEnv();
 
@@ -13,6 +13,7 @@ if (!command) {
 }
 
 let child;
+let shuttingDown = false;
 try {
   child = spawnCommand(command, args, {
     env: process.env,
@@ -24,12 +25,26 @@ try {
   process.exit(1);
 }
 
+function stopChild(signal) {
+  if (shuttingDown) return;
+  shuttingDown = true;
+
+  console.log(`[run-with-env] received ${signal}, stopping ${command}`);
+  killProcessTree(child);
+
+  setTimeout(() => process.exit(0), 1000).unref();
+}
+
 child.on("error", (error) => {
   console.error(`[run-with-env] failed to start ${command}: ${error.message}`);
   process.exit(1);
 });
 
 child.on("exit", (code, signal) => {
+  if (shuttingDown) {
+    process.exit(0);
+  }
+
   if (signal) {
     console.error(`[run-with-env] ${command} exited from signal ${signal}`);
     process.exit(1);
@@ -37,3 +52,6 @@ child.on("exit", (code, signal) => {
 
   process.exit(code ?? 0);
 });
+
+process.on("SIGINT", () => stopChild("SIGINT"));
+process.on("SIGTERM", () => stopChild("SIGTERM"));
